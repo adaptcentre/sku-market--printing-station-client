844eec7119b1891e20e9d085d4de91532afe2232
ivanbacher <ivan.bacher@gmail.com>
2021-08-31 15:02:33 +0200

    setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap-utilities.min.css';
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import Aurelia from 'aurelia';
 import { MyApp } from './my-app';
 
--- src/my-app.js
+++ src/my-app.js
@@ -1,3 +1,14 @@
+import { route } from 'aurelia';
+
+@route({
+  routes: [
+    { path: '', redirectTo: 'welcome' },
+    { id: 'welcome', path: 'welcome', component: import('./resources/routes/welcome'), title: 'Welcome' },
+    { id: 'printing', path: 'printing', component: import('./resources/routes/printing'), title: 'Printing' }
+  ]
+})
+
+
 export class MyApp {
-  message = 'Hello World!';
+
 }
