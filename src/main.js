a3a3923c3000fe560a1078da57aa3aa835e84677
ivanbacher <ivan.bacher@gmail.com>
2021-08-31 15:22:46 +0200

    added qr scanner

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import 'bootstrap/dist/css/bootstrap-reboot.min.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import 'bootstrap/dist/css/bootstrap-utilities.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap-utilities.min.css'
 
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import Aurelia from 'aurelia';
-import { MyApp } from './my-app';
+import Aurelia, { RouterConfiguration } from 'aurelia'
+import { MyApp } from './my-app'
 
+  
 Aurelia
+  .register(RouterConfiguration.customize())
   .app(MyApp)
-  .start();
+  .start()
